52d1a16cd313c163916a52df8267cc821d45d5e2
Andrik <andri@JEN-PC>
2025-03-15 14:46:27 -0600

    Primer commit.

---
+++ Laboratorio1_C20060/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_C20060.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
